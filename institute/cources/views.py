8fc052502101b3db5dcf28ac25672318855d5908
Aadeshkale <aadeshkale0@gmail.com>
2018-09-01 20:40:47 +0530

    maping url with views

--- institute/cources/views.py
+++ institute/cources/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    msg='<h1 style="color:red ; text-align: center; ">Working Fine :)</h1>'
+    return HttpResponse(msg)
--- institute/institute/urls.py
+++ institute/institute/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from cources import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('index',views.index)
 ]
