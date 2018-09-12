3fb7b1100e058f253547699a524c2d0159654b06
Aadeshkale <aadeshkale0@gmail.com>
2018-09-12 11:08:53 +0530

    SET(value mode)

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -10,7 +10,7 @@ class Emp(models.Model):
     def __str__(self):
         return self.name
 class Depart(models.Model):
-    ed=models.OneToOneField(Emp,on_delete=models.SET_NULL,null=True)
+    ed=models.OneToOneField(Emp,on_delete=models.SET(10),null=True)
     dname=models.CharField(max_length=10)
     def __str__(self):
       return self.dname
