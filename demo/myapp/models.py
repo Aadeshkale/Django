8f0a147a83459ca7c52505cac466c2cb9c836fbe
Aadeshkale <aadeshkale0@gmail.com>
2018-09-12 10:21:18 +0530

    Relationship modes example

---
+++ demo/myapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Emp(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.CharField(max_length=40)
+    mobile=models.IntegerField()
+    address=models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
+class Depart(models.Model):
+    ed=models.OneToOneField(Emp)
+    dname=models.CharField(max_length=10)
+    def __str__(self):
+      return self.dname
+
