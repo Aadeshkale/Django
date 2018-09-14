2774e7f9c2eef91e1a24878cd3b4d7c947802c44
Aadeshkale <aadeshkale0@gmail.com>
2018-09-14 11:11:49 +0530

    cascading mode

--- Reletionships/Rel/myapp/admin.py
+++ Reletionships/Rel/myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Student,Course
+
+# admin model to whole tabel content
+
+class adminStudent(admin.ModelAdmin):
+    list_display=['id','name','gender','address']
+class adminCourse(admin.ModelAdmin):
+    list_display=['id','cname','cfee','student']
 
 # Register your models here.
+admin.site.register(Student,adminStudent)
+admin.site.register(Course,adminCourse)
