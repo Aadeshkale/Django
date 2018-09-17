31f38ddd36a35bffcea326ac636b6d1403155c7e
Aadeshkale <aadeshkale0@gmail.com>
2018-09-17 22:03:18 +0530

    Many to Many reletionship models

--- 5.manyTomany/mtm/mtmapp/models.py
+++ 5.manyTomany/mtm/mtmapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name=models.CharField(max_length=20)
-    age=models.ImageField()
+    age=models.IntegerField()
     loc=models.CharField(max_length=20)
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Student(models.Model):
 class Course(models.Model):
     student=models.ManyToManyField(Student)
     cname=models.CharField(max_length=20)
-    cfee=models.ImageField()
+    cfee=models.IntegerField()
     def __str__(self):
         return self.cname
     
