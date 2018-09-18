2196cc8dd5e9d7e88b9f829402886ae6b86e5fb2
Aadeshkale <aadeshkale0@gmail.com>
2018-09-18 22:55:16 +0530

    register admin modells into admin site

--- 6.Inheritance-Abstract Model/Abin/Abapp/admin.py
+++ 6.Inheritance-Abstract Model/Abin/Abapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Student,Employee,Coustmer
 
+# creating admin model for to see hole table
+
+class AdminStudent(admin.ModelAdmin):
+    list_display=['name','loc','fee']
+class AdminCourse(admin.ModelAdmin):
+    list_display=['name','loc','sales']
+class AdminEmployee(admin.ModelAdmin):
+    list_display=['name','loc','salary']
+    
 # Register your models here.
+
+admin.site.register(Student,AdminStudent)
+admin.site.register(Coustmer,AdminCourse)
+admin.site.register(Employee,AdminEmployee)
