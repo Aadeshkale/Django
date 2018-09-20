87c325dc8ca81d1565cff5b2594785ce679df3fa
Aadeshkale <aadeshkale0@gmail.com>
2018-09-20 11:23:57 +0530

    Writing Mutilevel models

--- 7.inheritance-Multi-level/multilevel/mltil/myapp/models.py
+++ 7.inheritance-Multi-level/multilevel/mltil/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# multi-level inheritance 
+
+class Coustmer(models.Model):
+    cname=models.CharField(max_length=20)
+    csals=models.IntegerField()
+
+
+class Employee(Coustmer):                                    # inheriting propertie from Coustermer classs 
+    ename=models.CharField(max_length=20)                    # cname , csals  
+    esal=models.IntegerField()
+    
+class Student(Coustmer):                                     # inheriting properties of Coutermer,Employee class
+    sname=models.CharField(max_length=20)                    # cname, csals , ename , esal                           
+    sfee=models.IntegerField()
