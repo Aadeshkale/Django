d5bb9cea4ae2813b72a0fdab3d7d0dd0b05dfbbc
Aadeshkale <aadeshkale0@gmail.com>
2018-09-20 22:43:37 +0530

    multiple inheritance model note

--- 8.inheritance-multiple model/multiple/myapp/models.py
+++ 8.inheritance-multiple model/multiple/myapp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    eid=models.AutoField(primary_key=True)
+    eid=models.AutoField(primary_key=True)                   # solveing problem of default id field 
     name=models.CharField(max_length=20)
 class Coustmer(models.Model):
-    cid=models.AutoField(primary_key=True)
+    cid=models.AutoField(primary_key=True)                   # solveing problem of default id field               
     loc=models.CharField(max_length=20)    
 
 class Student(Employee,Coustmer):
