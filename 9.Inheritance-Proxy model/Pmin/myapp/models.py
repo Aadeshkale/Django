3ed82d3efa8c8173b3d2dd316d9ad067d4ab3307
Aadeshkale <aadeshkale0@gmail.com>
2018-09-22 10:08:09 +0530

    Migrations for proxy model

--- 9.Inheritance-Proxy model/Pmin/myapp/models.py
+++ 9.Inheritance-Proxy model/Pmin/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    sid=models.AutoField(primary=True)
+    sid=models.AutoField(primary_key=True)
     name=models.CharField(max_length=20)
     email=models.EmailField(max_length=50)
     address=models.CharField(max_length=40)
