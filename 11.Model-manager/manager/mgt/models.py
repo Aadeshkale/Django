e1dd17874f221f443c1bc139f52131da63d27937
Aadeshkale <aadeshkale0@gmail.com>
2018-09-24 23:01:12 +0530

    handling error for adding new field

--- 11.Model-manager/manager/mgt/models.py
+++ 11.Model-manager/manager/mgt/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Emp(models.Model):
     name=models.CharField(max_length=20)
     loc=models.CharField(max_length=20)
+    email=models.EmailField(max_length=20,default='sample@gamil.com')
+    obj=models.Manager()
+    def __str__(self):
+        return self.name
     
