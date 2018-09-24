ddf045b102caa267ab22b2d49de1995e31e54be1
Aadeshkale <aadeshkale0@gmail.com>
2018-09-24 22:44:47 +0530

    rename default manager 'objects' to 'obj'

--- 11.Model-manager/manager/mgt/models.py
+++ 11.Model-manager/manager/mgt/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-class Emp(models.Model):  
+class Emp(models.Model):
+         
       name=models.CharField(max_length=20)
       email=models.EmailField( max_length=20)
       loc=models.CharField(max_length=20)
+         
+      obj=models.Manager()               # rename default manager 'objects' to 'obj'  
       def __str__(self):
           return self.name
             
