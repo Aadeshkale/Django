488585f5abbd6e9233c26de2e512770b69a1a034
Aadeshkale <aadeshkale0@gmail.com>
2018-09-25 14:34:54 +0530

    mutiple inheritace

--- prac/multiple/myapp/admin.py
+++ prac/multiple/myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Emp,Cst,Std
+# Model Admin
+class AdminEmp(admin.ModelAdmin):
+    list_display=['eid','name','loc']
+class AdminCst(admin.ModelAdmin):
+    list_display=['cid','email','address']
+class AdminStd(admin.ModelAdmin):
+    list_display=['fee']    
 
 # Register your models here.
+
+admin.site.register(Emp,AdminEmp)
+admin.site.register(Cst,AdminCst)
+admin.site.register(Std,AdminStd)
--- prac/multiple/myapp/models.py
+++ prac/multiple/myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Emp(models.Model):
+    eid=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=20)
+    loc=models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Cst(models.Model):
+    cid=models.AutoField(primary_key=True)
+    email=models.EmailField(max_length=20)
+    address=models.CharField(max_length=20)
+    def __str__(self):
+        return self.email
+       
+class Std(Emp,Cst):
+    fee=models.IntegerField()
+    def __str__(self):
+        return self.name
+    
