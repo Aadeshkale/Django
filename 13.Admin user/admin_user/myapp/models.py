cbe5a49839f6043afcbb0b5d3c5bdb6ce2ddb1ae
Aadeshkale <aadeshkale0@gmail.com>
2018-09-28 12:05:49 +0530

    created user with add, view permissions

--- 13.Admin user/admin_user/myapp/models.py
+++ 13.Admin user/admin_user/myapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Emp(models.Model):
     name=models.CharField(max_length=20)
-    loc=models.IntegerField()
+    loc=models.CharField(max_length=20)
     address=models.CharField(max_length=20)
     def __str__(self):
         return self.name
