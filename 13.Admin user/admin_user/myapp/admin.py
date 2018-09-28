b8086b2cf5f98fa010ec9a248e9d151b7d26fd81
Aadeshkale <aadeshkale0@gmail.com>
2018-09-28 11:54:38 +0530

    migrations,superuser

--- 13.Admin user/admin_user/myapp/admin.py
+++ 13.Admin user/admin_user/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Emp
 
-# Register your models here.
+
+class AdminEmp(admin.ModelAdmin):
+    list_display=['id','name','loc','address']
+
+admin.site.register(Emp,AdminEmp)
