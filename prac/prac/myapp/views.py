4100a69bb0020122ebff49647211c81151a87535
Aadeshkale <aadeshkale0@gmail.com>
2018-10-01 14:40:21 +0530

    ok

--- prac/prac/myapp/views.py
+++ prac/prac/myapp/views.py
@@ -13,10 +13,16 @@ def index(request):
     for e in emp_list:
         response.write("<li><a href='/myapp/info/%d/'>%s</a></li>"%(e.id,e.name))
     response.write("</body></html>")
-
-    
-
-
-
-
     return HttpResponse(response)
+
+def details(request,eid):
+    response=HttpResponse()
+    response.write("<html><body>\n")
+    e=Emp.objects.get(id=eid)
+    response.write("<h1>Details of employee %s</h1>"%(e.name))
+    response.write("<h3>Name of employee: %s</h3>"%(e.name))
+    response.write("<h3>Email of employee: %s</h3>"%(e.email))
+    response.write("<h3>loc of employee: %s</h3>"%(e.loc))
+    response.write("<h3>sal of employee: %s</h3>"%(e.sal))
+    response.write("</body></html>")
+    return response
--- prac/prac/prac/urls.py
+++ prac/prac/prac/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from myapp.views import index
+from myapp.views import index,details
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',index),
+    url(r'^myapp/info/(\d+)/$',details),
 ]
