6bace7b39229db44908a014c71cb256f120fd274
Aadeshkale <aadeshkale0@gmail.com>
2018-10-06 00:22:40 +0530

    Addition app with app level urls

---
+++ Template/19.Addition app/addpro/addapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,'Addapp/add.html')
+    
+def addfun(request):
+    x=int(request.GET['val1'])
+    y=int(request.GET['val1'])
+    z=x+y;
+    s="your Addition is:",z
+    return HttpResponse(s)
