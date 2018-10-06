a2f5ba1e3bc3500e32b5a03ccddd2b3683bca8dc
Aadeshkale <aadeshkale0@gmail.com>
2018-10-06 23:20:50 +0530

    creating applevel urls

--- Template/20.Applevel url/mysite/books/views.py
+++ Template/20.Applevel url/mysite/books/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def python(request):
+    return render (request,'books/python.html')
+def django(request):
+    return render(request,'books/django.html')
