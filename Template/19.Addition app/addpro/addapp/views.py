acb8c872861c91168aad9d16131e97a0bbe39594
Aadeshkale <aadeshkale0@gmail.com>
2018-10-06 22:36:51 +0530

    Applevel url example

--- Template/19.Addition app/addpro/addapp/views.py
+++ Template/19.Addition app/addpro/addapp/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'Addapp/add.html')
-    
+
 def addfun(request):
     x=int(request.GET['val1'])
-    y=int(request.GET['val1'])
+    y=int(request.GET['val2'])
     z=x+y;
     s="your Addition is:",z
     return HttpResponse(s)
