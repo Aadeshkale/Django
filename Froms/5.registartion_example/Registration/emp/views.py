24cc50aeb7604e259ca8603f9de19450a0d7b2e2
Aadeshkale <aadeshkale0@gmail.com>
2018-10-12 23:32:35 +0530

    template for registration

--- Froms/5.registartion_example/Registration/emp/views.py
+++ Froms/5.registartion_example/Registration/emp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def emp(request):
+    if request.method=="POST":
+        pass
+    else:
+       return render(request,'emp.html')
+       
