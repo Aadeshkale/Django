46c3dd83a25c959a84cf644e294b06cc65e22059
Aadeshkale <aadeshkale0@gmail.com>
2018-10-13 00:33:02 +0530

    validation of data by is_valid

--- Froms/5.registartion_example/Registration/emp/views.py
+++ Froms/5.registartion_example/Registration/emp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .forms import EmpForm 
+from .forms import EmpForm
+from django.http import HttpResponse 
 def emp(request):
     if request.method=="POST":
-        pass
+        myform=EmpForm(request.POST)
+        if myform.is_valid():
+            return HttpResponse('Data inserted Sucessfully')
+        else:
+            return HttpResponse('Data is not Inserted')
+
+
     else:
         myform=EmpForm()
         return render(request,'emp.html',{'myform':myform})
