58f551a5d542ee7ecd6b36c8a6e3ecc98536009b
Aadeshkale <aadeshkale0@gmail.com>
2018-11-17 14:50:34 +0530

    inheritance abstract model practice

--- Practice/modelin/myapp/models.py
+++ Practice/modelin/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Common(models.Model):
+    name=models.CharField(max_length=20)
+    age=models.IntegerField()
+    def __str__(self):
+        return self.name
+    class Meta:
+        abstract=True
+
+class Student(Common,models.Model):
+    subject=models.CharField(max_length=20)
+
+class Coustmer(Common,models.Model):
+     payment=models.IntegerField()
+
