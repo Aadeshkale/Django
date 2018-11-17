911eff086ac6bc2348eb7d3a1cf97fc23aa14647
Aadeshkale <aadeshkale0@gmail.com>
2018-11-17 13:59:10 +0530

    one to many models reletionship practice

--- Practice/otm/myapp/admin.py
+++ Practice/otm/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from myapp.models import *
 # Register your models here.
+
+class  AdminBook(admin.ModelAdmin):
+    list_display=['id','name','year','price']
+
+class AdminAuthor(admin.ModelAdmin):
+    list_display=['book_id','name']
+
+admin.site.register(Book,AdminBook)
+admin.site.register(Author,AdminAuthor)
