6d62b0e480140ce7c48d5034326ce39e536450bd
Aadeshkale <aadeshkale0@gmail.com>
2018-11-17 13:55:00 +0530

    one to many models reletionship practice

--- Practice/otm/myapp/models.py
+++ Practice/otm/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name=models.CharField(max_length=30)
+    price=models.IntegerField()
+    year=models.IntegerField()
+
+class Author(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.DO_NOTHING)
+    name=models.CharField(max_length=20)
+
