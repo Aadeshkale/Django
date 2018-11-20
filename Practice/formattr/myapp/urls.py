77b9d245bfa7bf9620e99be990c9c7c141bba1ac
Aadeshkale <aadeshkale0@gmail.com>
2018-11-20 13:48:26 +0530

    Registration example

--- Practice/formattr/myapp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-from myapp.views import index
-urlpatterns = [
-    url(r'^$',index)
-]
--- Practice/regpro/regpro/wsgi.py
+++ Practice/regpro/regpro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for formattr project.
+WSGI config for regpro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formattr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'regpro.settings')
 
 application = get_wsgi_application()
