c2c3eae81fe2cf72589f9fd28ec3911b981775e6
Aadeshkale <aadeshkale0@gmail.com>
2018-11-20 12:39:37 +0530

    form methods --> GET and POST

--- Practice/formex/myapp/form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-class Myform(forms.Form):
-    name=forms.CharField()
-    email=forms.EmailField()
-    id=forms.IntegerField()
-    your_file=forms.FileField()
--- Practice/formex/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from myapp.form import Myform
-
-def index(request):
-    myform=Myform()
-    return render(request,'index.html',{'myform':myform})
