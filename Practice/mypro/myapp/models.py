d26982c0e7fdd480a1471dcab55d64a34a75dde1
Aadeshkale <aadeshkale0@gmail.com>
2018-11-21 15:10:07 +0530

    modelform done

--- Practice/mypro/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Reg(models.Model):
-    name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=20)
-    gender=models.CharField(max_length=20)
-    country=models.CharField(max_length=10)
