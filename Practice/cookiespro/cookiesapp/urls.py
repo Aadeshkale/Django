2c2d2f9ce265a5212078edebbcdf195f7f357965
Aadeshkale <aadeshkale0@gmail.com>
2018-11-21 16:01:03 +0530

    cookies done with visit count method

--- Practice/cookiespro/cookiesapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from cookiesapp.views import *
-urlpatterns = [
-    url(r'^$',index),
-    url(r'add/',add),
-    url(r'remove/',remove),
-    url(r'count/',count),
-]
