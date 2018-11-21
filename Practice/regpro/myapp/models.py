95548660d862c790f7c33500c19f9680bf72ce77
Aadeshkale <aadeshkale0@gmail.com>
2018-11-21 13:50:11 +0530

    modelform example

--- Practice/regpro/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Reg(models.Model):
-    name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=50)
-    gender=models.CharField(max_length=10)
-    country=models.CharField(max_length=10)
