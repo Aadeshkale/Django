12acd96916966c4865f4b2c37c33672b8db34415
Aadeshkale <aadeshkale0@gmail.com>
2018-11-24 14:45:52 +0530

    django crud oprations done

--- Practice/Crud/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from myapp.views import *
-urlpatterns = [
-    url(r'^$',index),
-    url(r'^insert',insert),
-    url(r'^display',display),
-    url(r'^update',update),
-    url(r'^delete',delete),
-]
