a84bf4413252118f82140cb74f185c648b725197
Aadeshkale <aadeshkale0@gmail.com>
2019-01-14 13:49:52 +0530

    form validation procedure

--- Practice/demopro/valapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ValappConfig(AppConfig):
-    name = 'valapp'
--- Practice/demopro/valapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Info(models.Model):
-    name=models.CharField(max_length=30)
-    email=models.EmailField(max_length=70)
-    
